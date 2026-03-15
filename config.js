a4f07b01255a59a682fcb258bfc23be7fdcabbad
varegina <varegina@nanosemantics.ai>
2026-03-15 23:01:33 +0300

    fix: open donats link

---
+++ config.js
@@ -0,0 +1,5 @@
+// Пример конфига с ссылкой на перевод.
+// Скопируйте этот файл в `config.js` и подставьте свою ссылку.
+
+window.PAYMENT_URL = "https://vtb.paymo.ru/collect-money/?transaction=350eaa69-a85f-4472-9521-30928f783570";
+
